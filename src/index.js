550d8879b865d0fc2a14bd645535a64ee0e8aa7d
Abu Zaid <zaid47304@gmail.com>
2022-05-20 22:05:52 +0530

    added navbar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import './App.css';
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-export const app = () => {
-  return (
-    <>
-    <h1>App</h1>
-    </>
-  )
-}
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+reportWebVitals();
