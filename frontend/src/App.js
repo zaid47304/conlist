7adcf60427c47522bfef13d7fd7db87570d161cc
Abu Zaid <zaid47304@gmail.com>
2022-05-29 12:32:57 +0530

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,29 @@ import Navbar from './components/Navbar';
 import Login from './components/login';
 import Signup from './components/signup';
 import Contest from './components/Contest';
+import Alert from './components/alert';
+import { useState } from 'react';
 function App() {
+  const [alert, setalert] = useState(null);
+  const showAlert = (message, type) => {
+    setalert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setalert(null);
+    }, 1500)
+  }
   return (
     <>
     <BrowserRouter>
     <Navbar/>
+    <Alert alert={alert} />
     <h2 className='text-center'>Con<span className="badge bg-secondary">List</span></h2>
     <Routes>
-    <Route exact path="/" element={<Contest/>}></Route>
-    <Route exact path="/login" element={<Login />}></Route>
-    <Route exact path="/signup" element={<Signup />}></Route>
+    <Route exact path="/" element={<Contest showAlert={showAlert}/>}></Route>
+    <Route exact path="/login" element={<Login showAlert={showAlert} />}></Route>
+    <Route exact path="/signup" element={<Signup showAlert={showAlert} />}></Route>
     </Routes>
     </BrowserRouter>
 
